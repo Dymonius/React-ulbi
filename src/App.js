21872a249ffaddeafa0e3d4ecafcf53597dfb686
Dvernikov Dmitriy <dymonius12@gmail.com>
2022-06-12 18:37:40 +0300

    Counter and Input were added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [likes, setLikes] = useState(0);
+    const [value, setValue] = useState('test');
+
+    function inc() {
+        setLikes(likes + 1);
+    }
+
+    function dec() {
+        setLikes(likes - 1);
+    }
+
+    return (
+        <div className="App">
+            <h1>{likes}</h1>
+            <button onClick={inc}>+</button>
+            <button onClick={dec}>-</button>
+            <h1>{value}</h1>
+            <input
+                type="text"
+                value={value}
+                onChange={e => setValue(e.target.value)}
+            />
+        </div>
+    );
 }
 
 export default App;
